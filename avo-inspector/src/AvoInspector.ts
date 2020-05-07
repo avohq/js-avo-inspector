bbac837a9066119dd2e829a107339386461d9726
Alexey Verein <tpom6oh@gmail.com>
2020-05-07 16:03:06 +0300

    Track sessions with localstorage

--- avo-inspector/src/AvoInspector.ts
+++ avo-inspector/src/AvoInspector.ts
@@ -1,21 +1,32 @@
 import { AvoInspectorEnv } from './AvoInspectorEnv';
 import { AvoSchemaParser } from './AvoSchemaParser';
+import { AvoSessionTracker } from './AvoSessionTracker';
 
 export class AvoInspector {
     
     environment: AvoInspectorEnv;
+    sessionTracker: AvoSessionTracker;
 
     constructor(apiKey: string, env: AvoInspectorEnv) {
         this.environment = env;
+
+        this.sessionTracker = new AvoSessionTracker({ startSession: () => {} });
+
+        let inspector = this;
+        window.onload = function() {
+            inspector.sessionTracker.startOrProlongSession(Date.now());
+        };
     }
     
     trackSchemaFromEvent(eventName: string, eventProperties: { [propName: string] : any}): { [propName: string] : string} {
         console.log('Inspected event: ' + eventName + ": " + JSON.stringify(eventProperties));
+        this.sessionTracker.startOrProlongSession(Date.now());
         return { "prop": "unknown" };
     }
 
     trackSchema(eventName: string, eventSchema: { [propName: string] : string}) {
         console.log('Inspected event: ' + eventName + ": " + JSON.stringify(eventSchema));
+        this.sessionTracker.startOrProlongSession(Date.now());
     }
 
     enableLogging(enable: Boolean) {
@@ -23,6 +34,7 @@ export class AvoInspector {
     }
     
     extractSchema(eventProperties: { [propName: string] : any}): string {
+        this.sessionTracker.startOrProlongSession(Date.now());
         return new AvoSchemaParser().extractSchema(eventProperties)
     }
 
---
+++ avo-inspector/src/Parsing.test.ts
@@ -0,0 +1,28 @@
+import { AvoInspector } from "./AvoInspector";
+import { AvoInspectorEnv } from "./AvoInspectorEnv";
+
+describe('Parsing', () => {
+  test('Event parsing', () => {
+    // Given
+    let eventProperties = { "prop0": true, "prop1": 1, "prop2": "str", 
+    "prop3": 0.5, "prop4": undefined, "prop5": null,
+    "prop6": { "an": "object" }, "prop7": ["a", "list", {"obj in list": true, "int field": 1}, ["another", "list"]] };
+
+    // When
+    let inspector = new AvoInspector("apiKey", AvoInspectorEnv.Dev);
+    let res = JSON.parse(inspector.extractSchema(eventProperties));
+
+    expect(res[0]["propertyValue"]).toBe("boolean");
+    expect(res[1]["propertyValue"]).toBe("int");
+    expect(res[2]["propertyValue"]).toBe("string");
+    expect(res[3]["propertyValue"]).toBe("float");
+    expect(res[4]["propertyValue"]).toBe("null");
+    expect(res[5]["propertyValue"]).toBe("null");
+    expect(res[6]["propertyValue"]).toBe("object");
+    expect(res[6]["children"]).toMatchObject([{"propertyName": "an", "propertyValue": "string"}]);
+
+    expect(res[7]["propertyValue"]).toBe("list");
+    expect(res[7]["children"]).toMatchObject(["string", [{"propertyName": "obj in list", "propertyValue": "boolean"},
+      {"propertyName": "int field", "propertyValue": "int"}], ["string"]]);
+  });
+});
