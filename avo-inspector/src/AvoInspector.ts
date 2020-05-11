d5fdd5b243c7c61c9540affc32db450d8b561370
Árni Hermann Reynisson <arnihr@gmail.com>
2020-05-11 13:55:38 +0000

    Add prettier

--- avo-inspector/src/AvoInspector.ts
+++ avo-inspector/src/AvoInspector.ts
@@ -1,34 +1,32 @@
-import { AvoInspectorEnv } from './AvoInspectorEnv';
-import { AvoSchemaParser } from './AvoSchemaParser';
+import { AvoInspectorEnv } from "./AvoInspectorEnv";
+import { AvoSchemaParser } from "./AvoSchemaParser";
 
 export class AvoInspector {
-    
-    constructor(apiKey: string, env: AvoInspectorEnv) {
+  constructor(apiKey: string, env: AvoInspectorEnv) {}
 
-    }
-    
-    trackSchemaFromEvent(eventName: string, eventProperties: { [propName: string] : any}): { [propName: string] : string} {
-        console.log('Inspected event: ' + eventName + ": " + JSON.stringify(eventProperties));
-        return { "prop": "unknown" };
-    }
+  trackSchemaFromEvent(
+    eventName: string,
+    eventProperties: { [propName: string]: any }
+  ): { [propName: string]: string } {
+    console.log(
+      "Inspected event: " + eventName + ": " + JSON.stringify(eventProperties)
+    );
+    return { prop: "unknown" };
+  }
 
-    trackSchema(eventName: string, eventSchema: { [propName: string] : string}) {
-        console.log('Inspected event: ' + eventName + ": " + JSON.stringify(eventSchema));
-    }
+  trackSchema(eventName: string, eventSchema: { [propName: string]: string }) {
+    console.log(
+      "Inspected event: " + eventName + ": " + JSON.stringify(eventSchema)
+    );
+  }
 
-    enableLogging(enable: Boolean) {
-        
-    }
-    
-    extractSchema(eventProperties: { [propName: string] : any}): string {
-        return new AvoSchemaParser().extractSchema(eventProperties)
-    }
+  enableLogging(enable: Boolean) {}
 
-    setBatchSize(newBatchSize: Number) {
+  extractSchema(eventProperties: { [propName: string]: any }): string {
+    return new AvoSchemaParser().extractSchema(eventProperties);
+  }
 
-    }
+  setBatchSize(newBatchSize: Number) {}
 
-    setBatchFlushSeconds(newBatchFlushSeconds: Number) {
-
-    }
+  setBatchFlushSeconds(newBatchFlushSeconds: Number) {}
 }
