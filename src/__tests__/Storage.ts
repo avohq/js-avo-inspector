b77c8d8c0a9f29efed32fd3aa8d1d0daf90c0a6d
Alexey Verein <tpom6oh@gmail.com>
2020-11-09 18:54:58 +0300

    Add storage tests w/wo logs

--- src/__tests__/Storage.ts
+++ src/__tests__/Storage.ts
@@ -1,14 +1,25 @@
 import { AvoStorage } from "../AvoStorage";
 import { defaultOptions } from "../__tests__/constants";
 
-
 describe("Avo Storage", () => {
   const storage = new AvoStorage(defaultOptions.shouldLog);
 
   const key = "avoKey";
   const value = "avoValue";
 
-  test("Sets and gets item", () => {
+  test("Sets and gets item with shouldLog on", () => {
+    const storage = new AvoStorage(true);
+
+    storage.setItem(key, value);
+
+    const item = storage.getItem(key);
+
+    expect(item).toBe(value);
+  });
+
+  test("Sets and gets item with shouldLog off", () => {
+    const storage = new AvoStorage(false);
+
     storage.setItem(key, value);
 
     const item = storage.getItem(key);
@@ -33,7 +44,19 @@ describe("Avo Storage", () => {
     expect(item).toBeNull();
   });
 
-  test("Deletes item", () => {
+  test("Deletes item with shouldLog on", () => {
+    const storage = new AvoStorage(true);
+
+    storage.removeItem(key);
+
+    const item = storage.getItem(key);
+
+    expect(item).toBeNull();
+  });
+
+  test("Deletes item with shouldLog off", () => {
+    const storage = new AvoStorage(false);
+
     storage.removeItem(key);
 
     const item = storage.getItem(key);
