5ed425d1432f2eaeb6d60257dc6e2e8cd4100875
Alexey Verein <tpom6oh@gmail.com>
2025-10-21 12:14:11 -0300

    Remove from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { AvoInspector } from "./AvoInspector";
 export { AvoInspectorEnv, type AvoInspectorEnvType, type AvoInspectorEnvValueType } from "./AvoInspectorEnv";
-export { AvoAnonymousId } from "./AvoAnonymousId";
