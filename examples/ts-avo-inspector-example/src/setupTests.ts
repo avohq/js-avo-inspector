3ec42453c4a9b494be047659706a55d370017467
Alexey Verein <tpom6oh@gmail.com>
2025-11-27 08:46:40 -0300

    Fix tests

--- examples/ts-avo-inspector-example/src/setupTests.ts
+++ examples/ts-avo-inspector-example/src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Polyfill TextEncoder/TextDecoder for Jest environment (required by eciesjs encryption library)
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
